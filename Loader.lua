813d75802dd3047e8853b09eb83cbbd0ac51f3e9
hankiwastaken <212605163+hankiwastaken@users.noreply.github.com>
2026-03-18 14:38:46 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,4 +9,40 @@
      Discord: dc.gg/shwxteam
 ]]
 
-loadstring(request({Url="https://risehub.fun/scripts/RiseRedirecter.lua",Method="GET"}).Body)()
+local HawkLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/hankiwastaken/HawkUILib/refs/heads/main/Source/HawkLib.lua"))() 
+
+_HawkKey = "pencizurnabayilirim"
+
+local Notif = HawkLib:AddNotifications()
+
+local success, data = pcall(function()
+	return request({
+		Url = "https://risehub.fun",
+		Method = "GET"
+	})
+end)
+
+if success then
+	--Successfull request
+	if data.Body:find("530") then
+		Notif:Notification({
+			Title = "Rise Server",
+			Description = "Rise server is down for some reason. Contact support.",
+			Selection = "Error",
+			Animated = true,
+			Cooldown = 5
+		})
+	elseif data.Body:find("502") then
+		Notif:Notification({
+			Title = "Rise Server",
+			Description = "Rise is currently down. Please try again later.",
+			Selection = "Error",
+			Animated = true,
+			Cooldown = 5
+		})
+	else
+		loadstring(request({Url="https://risehub.fun/scripts/RiseRedirecter.lua",Method="GET"}).Body)()
+	end
+else
+	warn("Failed to fetch site")
+end
