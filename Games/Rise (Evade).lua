ce1d53a0d7dcfb8bb18dc89069102b89afed2ddb
HzShadow <darknw211@gmail.com>
2026-04-20 21:58:55 +0300

    Update Rise (Evade).lua

--- Games/Rise (Evade).lua
+++ Games/Rise (Evade).lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ShadowBey01/SHWX-Team-Rise-Scripts/refs/heads/main/Loader.lua"))()
+loadstring(request({Url="https://risearmor.net/scripts/loader.lua",Method="GET"}).Body)()
